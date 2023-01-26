02bda4b81d401590fb611e4a46f2b606c461e08c
Kirill Kniga <kiruha_95@icloud.com>
2023-01-26 19:46:26 +0300

    Initial Commit

---
+++ Svetofor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Svetofor
+//
+//  Created by Kirill Kniga on 26.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
